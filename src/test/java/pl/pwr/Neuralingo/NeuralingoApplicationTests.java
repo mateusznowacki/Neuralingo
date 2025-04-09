3361606f8f4f627118d893cb7192e4f4b3eef019
matty <mattynovy@outlook.com>
2025-04-09 18:46:15 +0200

    init commit

---
+++ src/test/java/pl/pwr/Neuralingo/NeuralingoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pwr.Neuralingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuralingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
