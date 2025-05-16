9a5317bb30fa131c9b7125420c2a47f3d8eb79b2
mateusz-nowacki <mattynovy@outlook.com>
2025-05-16 17:31:27 +0200

    - open api config

--- src/main/java/pl/pwr/Neuralingo/NeuralingoApplication.java
+++ src/main/java/pl/pwr/Neuralingo/NeuralingoApplication.java
@@ -5,12 +5,6 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-@OpenAPIDefinition(
-		servers = @Server(url = "http://localhost:8080", description = "Local server")
-)
-
-
 @SpringBootApplication
 public class NeuralingoApplication {
 
