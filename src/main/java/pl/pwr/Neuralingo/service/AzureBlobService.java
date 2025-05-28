f578effb71b5fedad27f0b9612ac650b59536485
mateusz-nowacki <mattynovy@outlook.com>
2025-05-29 01:07:36 +0200

    azure blob service changes

--- src/main/java/pl/pwr/Neuralingo/service/AzureBlobService.java
+++ src/main/java/pl/pwr/Neuralingo/service/AzureBlobService.java
@@ -71,18 +71,6 @@ public class AzureBlobService {
             String relativePath = "resources/temp";
             File directory = new File(relativePath);
 
-            // Usuń wszystkie pliki z katalogu jeśli istnieje
-            if (directory.exists()) {
-                File[] files = directory.listFiles();
-                if (files != null) {
-                    for (File file : files) {
-                        if (!file.delete()) {
-                            System.err.println("❗ Nie udało się usunąć pliku: " + file.getName());
-                        }
-                    }
-                }
-            }
-
             if (!directory.exists() && !directory.mkdirs()) {
                 throw new RuntimeException("Nie można utworzyć katalogu: " + relativePath);
             }
